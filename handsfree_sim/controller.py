09d728620051220169460d1f40a695642fbbd450
Kalash <kalashjain513@gmail.com>
2023-05-18 23:08:34 +0530

    Used cv_bridge to show the camera footage

--- handsfree_sim/controller.py
+++ handsfree_sim/controller.py
@@ -4,8 +4,10 @@ import cv2 as cv
 import rclpy
 from rclpy.node import Node
 from handsfree_sim.brain import movement
+from cv_bridge import CvBridge
 
 from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Image
 
 class Controller(Node):
     
@@ -14,8 +16,12 @@ class Controller(Node):
         
         self.cap_ = cv.VideoCapture(0)
         self.timer_ = self.create_timer(0.1, self.timer_callback)
-        self.publisher_ = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
+        self.controller_ = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
+        self.image_publisher_ = self.create_publisher(Image, "frames", 10)
+        self.image_subscriber_ = self.create_subscription(Image, "frames", self.show_image, 10)
+        
         self.velocity_ = Twist()
+        self.br = CvBridge()
         
     def timer_callback(self):
         success, image = self.cap_.read()
@@ -38,9 +44,13 @@ class Controller(Node):
                     self.velocity_.linear.x = 0.0
                     self.velocity_.angular.z = 0.0
                 
-            self.publisher_.publish(self.velocity_)
-            cv.imshow('controller', image)
-        
+            self.controller_.publish(self.velocity_)
+            self.image_publisher_.publish(self.br.cv2_to_imgmsg(image))
+            
+    def show_image(self, data):
+        frame = self.br.imgmsg_to_cv2(data)
+        cv.imshow('Controller', frame)
+        cv.waitKey(1)
         
 def main(args=None):
     rclpy.init(args=args)
